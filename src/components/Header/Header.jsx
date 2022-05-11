746582254bc6e650af12bcd99f3d0e2a0cd1dd4d
pavankrishna333 <pavankrishnakamatham@gmail.com>
2022-05-11 13:09:43 +0530

    created card components from fetched data of travel-advisor api

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
-import SearchIcon from "@material-ui/icons/Search";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
-import useStyles from "./styles";
+import SearchIcon from "@material-ui/icons/Search";
 
-const Header = () => {
+import useStyles from "./styles.js";
+
+const Header = ({ onPlaceChanged, onLoad }) => {
   const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
@@ -14,22 +16,23 @@ const Header = () => {
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
-            Explore New Places
+            Explore new places
           </Typography>
-          {/*<Autocomplete>*/}
-          <div className={classes.search}>
-            <div className={classes.searchIcon}>
-              <SearchIcon />
+          <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
+            <div className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase
+                placeholder="Search…"
+                classes={{ root: classes.inputRoot, input: classes.inputInput }}
+              />
             </div>
-            <InputBase
-              placeholder="Search..."
-              classes={{ root: classes.inputRoot, input: classes.inputInput }}
-            ></InputBase>
-          </div>
-          {/*</Autocomplete>*/}
+          </Autocomplete>
         </Box>
       </Toolbar>
     </AppBar>
   );
 };
+
 export default Header;
